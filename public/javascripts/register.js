3874a67aed79c4aabd9c173a9b3410a1d96591a2
dacardenasa <diego.cardenasaleg@gmail.com>
2020-09-03 15:10:28 -0500

    Add password validations

--- public/javascripts/register.js
+++ public/javascripts/register.js
@@ -1,7 +1,7 @@
 function getInputErros(data) {
   let inputErrors = {};
   for (const input in data) {
-    if ( data[input].length === 0 ) {
+    if (data[input].length === 0) {
       inputErrors[input] = true;
     }
   }
@@ -9,11 +9,13 @@ function getInputErros(data) {
 }
 
 function matchPasswords(firstPass, secondPass) {
-  if (firstPass === secondPass) { return true }
+  if (firstPass === secondPass) {
+    return true;
+  }
   return false;
 }
 
-$("#btn-submit").on("click", function(event){
+$("#btn-submit").on("click", function (event) {
   const email = $("#email").val();
   const password = $("#password").val();
   const matchPassword = $("#matchPassword").val();
@@ -21,36 +23,47 @@ $("#btn-submit").on("click", function(event){
   const errors = getInputErros({ email, password, matchPassword });
   const passMatch = matchPasswords(password, matchPassword);
 
-  if ( Object.keys(errors).length === 0 && passMatch ) {
+  if (Object.keys(errors).length === 0 && passMatch) {
     const user = {
       email,
-      password
-    }
-    
+      password,
+    };
+
     $.ajax({
       type: "POST",
       url: "/",
       data: user,
       dataType: "json",
-      success: function(data) {
+      success: function (data) {
         if (data.status === 200) {
           $("#message-box p").text(data.message);
-          $("#message-box").removeClass("hide-note").css({ "display": "block" });
-          $("#email").val('');
-          $("#password").val('');
-          $("#matchPassword").val('');
+          $("#message-box").removeClass("hide-note").css({ display: "block" });
+          $("#email").val("");
+          $("#password").val("");
+          $("#matchPassword").val("");
         } else {
-          console.log(data.message)
+          console.log(data.message);
           $("#message-box p").text(data.message);
-          $("#message-box").removeClass("hide-note alert-success").addClass("alert-danger").css({ "display": "block" });
+          $("#message-box")
+            .removeClass("hide-note alert-success")
+            .addClass("alert-danger")
+            .css({ display: "block" });
         }
       },
-    })
-
+    });
   } else {
     for (const error in errors) {
       $(`#${error}`).addClass("is-invalid");
-      $(`#${error}Help`).removeClass("hide-note").css({ "display": "block" });
+      $(`#${error}Help`).removeClass("hide-note").css({ display: "block" });
+    }
+
+    if (!passMatch) {
+      $("#passwordHelp").text("Password doesn't match!");
+      $("#matchPasswordHelp").text("Password doesn't match!");
+      $("#password").addClass("is-invalid");
+      $("#passwordHelp").removeClass("hide-note").css({ display: "block" });
+      $("#matchPassword").addClass("is-invalid");
+      $("#matchPasswordHelp").removeClass("hide-note").css({ display: "block" });
     }
   }
   event.preventDefault();
